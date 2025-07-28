5ba85ab8e73c125aa3a5290a78790b59fd2f0bcb
Rich Storm <rich4storm@gmail.com>
2025-07-28 19:11:30 +0700

    fix: update root page to use new landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { LandingPage } from '@/components/landing-page';
+import { NewLandingPage } from '@/components/new-landing-page';
 
 export default function Home() {
-  return <LandingPage />;
+  return <NewLandingPage />;
 }
